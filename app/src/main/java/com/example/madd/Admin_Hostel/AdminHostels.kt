0149cc489b91622808a7678f70c2a56a7fa28ae9
msi <it21299766@my.sliit.lk>
2023-05-06 16:03:44 +0530

    login

--- app/src/main/java/com/example/madd/Admin_Hostel/AdminHostels.kt
+++
@@ -1,9 +0,0 @@
-package com.example.madd.Admin_Hostel
-
-data class AdminHostels(
-    var key: String? = null,
-    val phone: Any? = null,
-    val img: Any? = null,
-    val name: Any? = null,
-    val description :Any? = null,
-)
