35c9bcd3b2294b1448baa0aa4ea17ee1cdbfcaaa
it21344824_akidu <it21344824@my.sliit.lk>
2023-05-06 12:16:16 +0530

    3rd

--- app/src/main/java/com/example/madd/screens/SignUpActivity.kt
+++ app/src/main/java/com/example/madd/screens/SignUpActivity.kt
@@ -12,63 +12,6 @@ import com.google.firebase.database.FirebaseDatabase
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignUpBinding
-    private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var database : DatabaseReference
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        firebaseAuth = FirebaseAuth.getInstance()
-
-        binding.textView.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val pass = binding.passET.text.toString()
-            val confirmPass = binding.confirmPassEt.text.toString()
-
-            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
-                if (pass == confirmPass) {
-                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val currentUser = firebaseAuth.currentUser
-                            val databaseReference = FirebaseDatabase.getInstance().getReference("Users")
-                            val user = User(email = email,pass = pass, role = "user")
-                            currentUser?.uid?.let { uid ->
-                                databaseReference.child(uid).setValue(user).addOnCompleteListener { task ->
-                                    if (task.isSuccessful) {
-                                        val intent = Intent(this, LoginActivity::class.java)
-                                        startActivity(intent)
-                                    } else {
-                                        Toast.makeText(this, "Error: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
-                                    }
-                                }
-                            }
-                        } else {
-                            Toast.makeText(this, it.exception?.message ?: "Unknown error", Toast.LENGTH_SHORT).show()
-                        }
-
-                    }
-                } else {
-                    Toast.makeText(this, "password is not match", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Empty field are not allowed", Toast.LENGTH_SHORT).show()
-            }
-
-        }
-
-
-    }
 }
 
